d92123e8b0c7ee722b55bf69d9defff99d751c6d
hamza.elbouazzaoui <hamza.elbouazzaoui@sully-group.com>
2023-06-11 22:07:03 +0100

    Adding Master Page

--- src/app/_shared/shared.module.ts
+++ src/app/_shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideBarComponent,
+    LayoutComponent,
+    NavBarComponent
+  ],
   imports: [
     CommonModule,
   ],
--- src/app/application/application-routing.module.ts
+++ src/app/application/application-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {LayoutComponent} from "../_shared/components/layout/layout.component";
 
+const routes : Routes =[
+  {
+    path : "",
+    component : LayoutComponent,
+    children :[
+      //{path :"admin" , component : }
+    ]
+  }
+]
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild(routes)
+  ],
+  exports :[RouterModule]
 })
 export class ApplicationRoutingModule { }
